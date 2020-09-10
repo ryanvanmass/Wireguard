2bca51394578083a068a6ff240ff1ce190f95b5d
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2020-09-09 21:46:28 -0400

    Client Bug Fix

--- Client/setup.sh
+++ Client/setup.sh
@@ -25,15 +25,15 @@ echo "what would you like the Client IP to be (10.100.100.?/32)?"
 read ClientIP
 
 #Configure File
-sudo su -c "echo \"[Interface]\" >> wg0-client.conf"
-sudo su -c "echo \"Address = 10.100.100.$ClientIP/32 \" >> wg0-client.conf"
-sudo su -c "echo \"PrivateKey = $PrivateKey\" >> wg0-client.conf"
-sudo su -c "echo \" \" >> wg0-client.conf"
-sudo su -c "echo \"[Peer]\" >> wg0-client.conf"
-sudo su -c "echo \"PublicKey = $ServerKey\" >> wg0-client.conf"
-sudo su -c "echo \"Endpoint = $ServerIP:51820\" >> wg0-client.conf"
-sudo su -c "echo \"AllowedIPs = 0.0.0.0/0\" >> wg0-client.conf"
-sudo su -c "echo \"PersistentKeepalive = 21\" >> wg0-client.conf"
+sudo su -c "echo \"[Interface]\" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \"Address = 10.100.100.$ClientIP/32 \" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \"PrivateKey = $PrivateKey\" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \" \" >> wg0-/etc/wireguard/client.conf"
+sudo su -c "echo \"[Peer]\" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \"PublicKey = $ServerKey\" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \"Endpoint = $ServerIP:51820\" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \"AllowedIPs = 0.0.0.0/0\" >> /etc/wireguard/wg0-client.conf"
+sudo su -c "echo \"PersistentKeepalive = 21\" >> /etc/wireguard/wg0-client.conf"
 
 ############### Start WireGuard ###############
 sudo wg-quick up wg0-client
