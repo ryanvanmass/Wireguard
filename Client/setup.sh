5173684769ab51a325595433b2477859a23f0d8e
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2020-09-09 21:17:31 -0400

    Client Bux Fix

--- Client/setup.sh
+++ Client/setup.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #!/bin/bash
 ############### Dependancies Install ###############
-sudo -i
 sudo add-apt-repository ppa:wireguard/wireguard
 sudo apt-get update
 sudo apt-get install -y wireguard-dkms wireguard-tools linux-headers-$(uname -r)
 
 ############### Generate Certificates ###############
  sudo su -c "wg genkey | tee /etc/wireguard/client_private_key | wg pubkey > /etc/wireguard/client_public_key"
- 
+
 ############### Create Configuration File ###############
 #Creates the File
-sudo su -c "touch wg0-client.conf"
+sudo su -c "touch /etc/wireguard/wg0-client.conf"
 
 #Variables for next step
-PrivateKey=$(cat client_private_key)
+PrivateKey=$(cat /etc/wireguard/client_private_key)
 
 echo "What is the Server Public Key?"
 read ServerKey
@@ -37,5 +36,5 @@ sudo su -c "echo \"AllowedIPs = 0.0.0.0/0\" >> wg0-client.conf"
 sudo su -c "echo \"PersistentKeepalive = 21\" >> wg0-client.conf"
 
 ############### Start WireGuard ###############
-sudo wg-quack up wg0-client
-sudo systemctl enable wg-quack@wg0-client
+sudo wg-quick up wg0-client
+sudo systemctl enable wg-quick@wg0-client
