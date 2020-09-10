4e11df9038eeafffc36f632d3531e32f70c28190
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2020-09-09 20:52:59 -0400

    Client Script Bug Fix

--- Client/setup.sh
+++ Client/setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #!/bin/bash
 ############### Dependancies Install ###############
+sudo -i
 sudo add-apt-repository ppa:wireguard/wireguard
 sudo apt-get update
-sudo apt-get install wireguard-dkms wireguard-tools linux-headers-$(uname -r)
+sudo apt-get install -y wireguard-dkms wireguard-tools linux-headers-$(uname -r)
 
 ############### Generate Certificates ###############
-cd /etc/wireguard/
+sudo su -c "cd /etc/wireguard/"
 pwd
-wg genkey | tee client_private_key | wg pubkey > client_public_key
+sudo su -c "wg genkey | tee client_private_key | wg pubkey > client_public_key"
 
 ############### Create Configuration File ###############
 #Creates the File
-touch wg0-client.conf
+sudo su -c "touch wg0-client.conf"
 
 #Variables for next step
 PrivateKey=$(cat client_private_key)
@@ -27,15 +28,15 @@ echo "what would you like the Client IP to be (10.100.100.?/32)?"
 read ClientIP
 
 #Configure File
-echo "[Interface]" >> wg0-client.conf
-echo "Address = 10.100.100.$ClientIP/32" >> wg0-client.conf
-echo "PrivateKey = $PrivateKey" >> wg0-client.conf
-echo " " >> wg0-client.conf
-echo "[Peer]" >> wg0-client.conf
-echo "PublicKey = $ServerKey" >> wg0-client.conf
-echo "Endpoint = $ServerIP:51820" >> wg0-client.conf
-echo "AllowedIPs = 0.0.0.0/0" >> wg0-client.conf
-echo "PersistentKeepalive = 21" >> wg0-client.conf
+sudo su -c "echo \"[Interface]\" >> wg0-client.conf"
+sudo su -c "echo \"Address = 10.100.100.$ClientIP/32 \" >> wg0-client.conf"
+sudo su -c "echo \"PrivateKey = $PrivateKey\" >> wg0-client.conf"
+sudo su -c "echo \" \" >> wg0-client.conf"
+sudo su -c "echo \"[Peer]\" >> wg0-client.conf"
+sudo su -c "echo \"PublicKey = $ServerKey\" >> wg0-client.conf"
+sudo su -c "echo \"Endpoint = $ServerIP:51820\" >> wg0-client.conf"
+sudo su -c "echo \"AllowedIPs = 0.0.0.0/0\" >> wg0-client.conf"
+sudo su -c "echo \"PersistentKeepalive = 21\" >> wg0-client.conf"
 
 ############### Start WireGuard ###############
 sudo wg-quack up wg0-client
